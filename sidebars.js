47b6b6368921659472af138b53d491b21e481b3a
hsiang <hsiang.tang@cybertan.com.tw>
2023-11-24 10:38:34 +0800

    first commit

---
+++ sidebars.js
@@ -0,0 +1,27 @@
+// @ts-check
+
+const sidebars = {
+  tutorialSidebar: [
+    'Intro',
+    {
+      type: 'category',
+      label: 'AI',
+      items: ['AI/Docusaurus_test', 'AI/test2', 'AI/test3'],
+    },
+    {
+      type: 'category',
+      label: 'BI',
+      items: ['BI/bbb'],
+    },
+    /* 註釋開始
+    {
+      type: 'category',
+      label: 'About Me',
+      items: ['About Me/about'], // 確保這裡是正確的相對路徑
+    },
+    註釋結束 */
+    
+  ],
+};
+
+export default sidebars;
